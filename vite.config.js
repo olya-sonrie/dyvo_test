a692289d469d7ac7d71c9e451af1520b259fe2b9
Olya Zaritska <sonrie@Sonries-MacBook.local>
2024-01-21 14:21:59 +0200

    fix config file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/olya-sonrie.github.io/dyvo_test/',
-    assetsDir: 'assets',
-    server: {
-        port: 3000,
-    },
+  base: '/olya-sonrie.github.io/dyvo_test/',
+  assetsDir: 'assets',
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
